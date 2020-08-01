0523377179fcacdbeb09f71249726f7d4d98ca3c
physmath17 <krishnajalan.nitr@gmail.com>
2020-08-01 19:43:00 +0530

    minor changes

--- Lagrange_interpolation/Lagrange_interpolation.f90
+++ Lagrange_interpolation/Lagrange_interpolation.f90
@@ -1,30 +1,33 @@
 program Lagr_interpol
     implicit none
 
-    integer::i,j,k,n
-    real::a,L=0.0,p=1.0
-    real,dimension(3)::x
+    integer :: i, j, k, n
+    real :: a, L = 0.0, p = 1.0
+    real,dimension(:), allocatable :: x
 
     print*, "Enter the no. of input values: "
     read*, n
+    
+    allocate(x(n))
+    
     print*, "Enter the input data points: "
-    do i = 1, 3
+    do i = 1, n
         read*, x(i)
     enddo
-    print*,"Enter the no. value at which the function is to be approximated: "
+    print*,"Enter the value at which the function is to be approximated: "
     read*, a
 
-    do j = 1, 3
+    do j = 1, n
         p = 1.0
-        do k = 1, 3
+        do k = 1, n
             if(j /= k) then
                 p = p * ((a - x(k))/(x(j) - x(k)))
             endif
             enddo
-        L = L + p * log(real(x(j)))
+        L = L + p * log(real(x(j)))			! estimating the value of log at 'a'
     enddo
 
-    print*, "The required value is :", L
-    print*, abs(real(log(2.0) - L))/log(2.0)
+    print*, "The required value is : ", L
+    print*, abs(real(log(a) - L))/log(a)
 
 end program Lagr_interpol
--- Lagrange_interpolation/interpolation.f90
+++ Lagrange_interpolation/interpolation.f90
@@ -8,7 +8,7 @@ program lagrange_interpolation
 		subroutine interpol(x, xp, yp, y)
 			real, intent(in) :: x
 			real, intent(out) :: y
-			real, dimension(:), intent(in) :: xp, yp
+			real, dimension(1:5), intent(in) :: xp, yp
 			integer :: j, k
 			real :: p, L = 0
 		end subroutine interpol
@@ -18,30 +18,32 @@ program lagrange_interpolation
 	y1 = (/0.5, 1.5, 2.5, 3.5, 4.5/)
 	a = (/1.5, 2.5/)
 	call interpol(a(1), x1, y1, val_a(1))
+	call interpol(a(2), x1, y1, val_a(2))
 	
-	print*, val_a(1)
+	print*, a
+	print*, val_a
 
-end program interp
+end program lagrange_interpolation
 	
 subroutine interpol(x, xp, yp, y)
 	implicit none
 	
 	real, intent(in) :: x		                ! point at which we need to interpolate
 	real, intent(out) :: y		                ! the interpolated value
-	real, dimension(:), intent(in) :: xp, yp	! given data points
+	real, dimension(1:5), intent(in) :: xp, yp	! given data points
 	integer :: arsize, j, k
 	real :: p, L = 0
 	
 	arsize = size(xp)
 ! this loops execute the Lagrange interpolation algorithm
-	 do j = 1, arsize
-        p = 1.0
-        do k = 1, arsize
-            if(j /= k) then
-                p = p * ((x - xp(k))/(xp(j) - xp(k)))
-            endif
-            enddo
-        L = L + p *yp(j)
+	do j = 1, arsize
+		p = 1.0
+		do k = 1, arsize
+			if(j /= k) then
+				p = p * ((x - xp(k))/(xp(j) - xp(k)))
+			endif
+		enddo
+		L = L + p * yp(j)
 	enddo
 	y = L
 end subroutine interpol
